8e4cb48717a725e48e0a8ede275c900dc10fb4d4
Anandha Sudhan <rasudhan@gmail.com>
2018-10-17 17:15:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line(deli)
+end
+
+def take_a_number(deli,customer)
+  deli.push(customer)
+  return "Welcome, #{customer}. You are number #{deli.length-1} in line."
+  
+end
+
+def now_serving(deli)
+end
